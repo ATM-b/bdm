dcbb34d0d107541fc11d0f32efb27c60bdf32446
ATM-b <temitopemichael619@gmail.com>
2025-08-30 21:23:53 -0700

    upload of bddm2

--- effects.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const menu = document.querySelector(".Menu");
-
-  hamburger.addEventListener("click", () => {
-    menu.classList.toggle("active");
-  });
-});
